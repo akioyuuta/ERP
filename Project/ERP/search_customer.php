69b31b0f49a806f436dec7aeeb45170a6dfe3236
Joseph Budiarto <Joseph Budiarto>
2016-11-25 09:53:21 +0700

    Search tak ganti

--- Project/ERP/search_customer.php
+++ Project/ERP/search_customer.php
@@ -1,7 +1,6 @@
 <?php include "connect.php";
 	$key = $_POST['key'];
-	$query = mysqli_query($con , "SELECT * FROM customer WHERE name LIKE '%".$key."%'");
-
+	$query = mysqli_query($con , "SELECT * FROM customer WHERE name LIKE '%".$key."%' OR city LIKE '%".$key."%' OR country LIKE '%".$key."%' OR email LIKE '%".$key."%'");
 	while($row = mysqli_fetch_assoc($query)){?>
 		<div class="container floatable" onclick=location.href="detail_customer.php?id=<?php echo $row['id'];?>";>
 			<div class="col-sm-3 col-xs-4"><i class="fa fa-user fa-5x"></i></div>
